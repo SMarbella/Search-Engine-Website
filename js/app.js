1caad78ff3a7050f874fe657966a7fc90070f5ff
SMarbella <swmarbella@email.arizona.edu>
2025-05-31 16:35:24 -0700

    Updated interface.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
 import { createRoot } from 'react-dom/client';
+import React from 'react';
+import { DatePicker } from 'antd';
 
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
+
+function Homepage() {
+return (
+<div>
+<h1>Welcome to My React Website</h1>
+<p>This is the homepage of my first React website!</p>
+</div>
+);
+}
+
+export default Homepage;
+
 
 function SearchButton() {
   function handleClick() {
