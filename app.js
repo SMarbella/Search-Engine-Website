bb4c48eb39f894938b0cc5a73653490758a40cef
SMarbella <swmarbella@email.arizona.edu>
2025-05-31 15:06:33 -0700

    Test React.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
 
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
+
 function SearchButton() {
   function handleClick() {
     alert('Search feature coming soon...');
   }
-  
+
   return (
     <button onClick={handleClick}>
       Search
